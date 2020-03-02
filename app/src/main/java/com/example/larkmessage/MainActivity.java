b5ec1be46080fad6c50dd2f9f3738273fff1a52e
guangen <19107773@studentmail.ul.ie>
2020-03-02 13:26:54 +0000

    after login

--- app/src/main/java/com/example/larkmessage/MainActivity.java
+++ app/src/main/java/com/example/larkmessage/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.larkmessage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,8 +23,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private static final  int RC_SIGN_IN =123;
+    private FirebaseAuth mAuth;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -45,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user==null)
+        {
+            loginFcn();
+        }
     }
 
     @Override
@@ -59,5 +76,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    public void loginFcn()
+    {
+        List<AuthUI.IdpConfig> list= Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(list).build(),RC_SIGN_IN
+        );
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==RC_SIGN_IN)
+        {
+            IdpResponse idpResponse =IdpResponse.fromResultIntent(data);
+            if(resultCode==RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                System.out.println("successful"+user.getDisplayName()+user.getEmail()+user.getPhoneNumber());
+                //Intent intent = new Intent(MainActivity.this,readingActicity.class);
+                //intent.putExtra(USERCODE,auth);
+                //startActivity(intent);
+            }
+            else
+            {
+                if(idpResponse ==null)
+                {
+                    System.out.println("Sign in cancelled");
+                    loginFcn();
+                    return;
+                }
+                if(idpResponse.getError().getErrorCode()== ErrorCodes.NO_NETWORK)
+                {
+                    System.out.println("no internet connection");
+                    loginFcn();
+                    return;
+                }
+            }
+        }
     }
 }
